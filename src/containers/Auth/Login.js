d240870196250cc0af91ae46e398a85ba50b8f94
TuanAnh-Seta <anhnt6879@seta-international.vn>
2022-12-14 12:18:24 +0100

    first commit

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from "connected-react-router";
 
-import * as actions from "../store/actions";
-import { KeyCodeUtils, LanguageUtils } from "../utils";
+import * as actions from "../../store/actions";
+import { KeyCodeUtils, LanguageUtils } from "../../utils";
 
-import userIcon from '../../src/assets/images/user.svg';
-import passIcon from '../../src/assets/images/pass.svg';
+import userIcon from '../../assets/images/user.svg';
+import passIcon from '../../assets/images/pass.svg';
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
 
-import adminService from '../services/adminService';
+import adminService from '../../services/adminService';
+import userService from '../../services/userService'
 
 class Login extends Component {
     constructor(props) {
